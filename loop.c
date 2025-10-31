bc5ed62a227b9b899cd49a4639c320930a03074c
Mehak2912 <MEHAK.22835@stu.upes.ac.in>
2025-10-31 22:49:49 +0530

    Create loop2.c

---
+++ loop.c
@@ -0,0 +1,12 @@
+#print the table of a number given by user
+
+#include <stdio.h>
+int main(){
+    int n;
+    printf("enter number:");
+    scanf("%d",&n);
+    for(int i=1;i<=10;i++){
+        printf("%d\n", n*i);
+    }
+    return 0;
+}
